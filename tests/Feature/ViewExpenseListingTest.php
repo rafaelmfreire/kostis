0e10da19a9bc8b5c0b562c8ef7d34e3582bfcb9c
Rafael de Macêdo <r4faelmf@gmail.com>
2022-11-15 22:35:01 -0300

    wip:expense listing feature

---
+++ tests/Feature/ViewExpenseListingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Expense;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ViewExpenseListingTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function user_can_view_an_expense_listing()
+    {
+        $this->withoutExceptionHandling();
+
+        //Arrange
+        $expense = Expense::create([
+            'source' => 'Nubank',
+            'date' => Carbon::parse('2022-11-15'),
+            'cost' => '2500',
+            'description' => 'Supermarket',
+            'category' => 'groceries',
+            'observation' => 'food and higiene'
+        ]);
+
+        //Act
+        $response = $this->get('/expenses');
+    
+        //Assert
+        $response->assertStatus(200);
+        //see the expense details
+        $response->assertSee('Nubank');
+        $response->assertSee('15/11/2022');
+        $response->assertSee('R$ 25,00');
+        $response->assertSee('Supermarket');
+        $response->assertSee('groceries');
+        $response->assertSee('food and higiene');
+    }
+}
