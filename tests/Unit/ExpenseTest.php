7b534c92e25ed748e7574b41fe33a1a7fdcd591f
Rafael de Macêdo <r4faelmf@gmail.com>
2022-11-18 14:55:00 -0300

    refactor to improve test speed

--- tests/Unit/ExpenseTest.php
+++ tests/Unit/ExpenseTest.php
@@ -5,21 +5,18 @@ namespace Tests\Unit;
 use App\Models\Expense;
 use App\Models\User;
 use Carbon\Carbon;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ExpenseTest extends TestCase
 {
-    use DatabaseMigrations;
-
     /** @test */
     public function can_get_formatted_date()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->make();
 
-        $expense = Expense::factory()->create([
+        $expense = Expense::factory()->make([
             'user_id' => $user->id,
-            'date' => Carbon::parse('2022-11-18')
+            'date' => Carbon::parse('2022-11-18'),
         ]);
 
         $date = $expense->formatted_date;
