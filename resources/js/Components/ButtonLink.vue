f5b1e5080b3774f3e03d92d6e9d64b4f370fd7c2
Rafael de Macêdo <r4faelmf@gmail.com>
2022-11-25 10:43:41 -0300

    create new button link component

---
+++ resources/js/Components/ButtonLink.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+const props = defineProps(['href']);
+
+</script>
+
+<template>
+    <Link :href="href" class="inline-flex items-center px-4 py-2 bg-indigo-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+        <slot />
+    </Link>
+</template>
