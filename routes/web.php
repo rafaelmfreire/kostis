5299f9c6fa1f8f72b7b164e7dbb6203fd0fc0f73
Rafael de Macêdo <r4faelmf@gmail.com>
2023-08-26 15:25:06 -0300

    adapt to multi user

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,15 @@ use Inertia\Inertia;
 |
 */
 
-Route::redirect('/', '/login');
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+// Route::redirect('/', '/login');
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
